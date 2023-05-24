085b80ddecbadd78c757c2a32f687a6d9f419996
Ben Gruey <91962409+bgruey@users.noreply.github.com>
2023-05-24 09:41:58 -0500

    bit of refactor

--- pc_client/test/integration/fixture_example_client.py
+++ pc_client/test/integration/fixture_example_client.py
@@ -2,7 +2,7 @@ import os
 
 import pytest
 
-from pc_client.client import BaseClientConfig
+from client.client import BaseClientConfig
 from example_protocol.example_client import Client
 from pc_protocol.error import ErrorResponse
 from pc_protocol.mresponses import ShutdownResponse
